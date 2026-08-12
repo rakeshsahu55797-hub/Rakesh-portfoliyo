43264f12601c52ac9139288b6c24ee9d4c8825e7
rakeshsahu55797-hub <rakeshsahu55797@gmail.com>
2026-08-12 21:16:37 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
